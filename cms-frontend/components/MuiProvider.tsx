7ae8656f14f9c24d0f25fb265e166169744314c4
Nick Mathieu <rakotomanananick29@gmail.com>
2026-02-23 16:36:05 +0300

    [front] use MUI Provider

---
+++ cms-frontend/components/MuiProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#0f172a",
+    },
+  },
+});
+
+export default function MuiProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
